020f14c0d91c281add19ca3b9ff3b684362428bd
Simab Rehman <simabrehman@yahoo.ca>
2026-02-02 17:25:43 -0500

    First day initial changes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -15,8 +15,8 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+        <Stack.Screen name="index" options={{ title: "Notes App" }} />
+        <Stack.Screen name="add" options={{ title: "Add note" }} />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
